52c955707039eb17c33e0a4fc850406d5b6f4a6b
Yogendra Singh <000yogi1990@gmail.com>
2019-11-26 09:45:50 +0530

    Initial Commit

---
+++ AccessibilitySample/CustomViews/CustomButtons/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  AccessibilitySample
+//
+//  Created by Yogendra Singh on 25/11/19.
+//  Copyright © 2019 Yogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AccessibilitySample/CustomViews/CustomTextFields/BottomLinedTextField.swift
@@ -0,0 +1,21 @@
+//
+//  BottomLinedTextField.swift
+//  AccessibilitySample
+//
+//  Created by Yogendra Singh on 25/11/19.
+//  Copyright © 2019 Yogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class BottomLinedTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
